a093c49ca099a664c2d3c309031e18f7f8f3086f
Rophy Tsai <rophy@users.noreply.github.com>
2022-02-08 18:50:43 +0800

    updated example workflow with nats-temporal-bridge usages

--- example/src/activities.ts
+++ example/src/activities.ts
@@ -1,3 +1,17 @@
+import { Context } from '@temporalio/activity'
+import axios from 'axios';
+
 export async function greet(name: string): Promise<string> {
   return `Hello, ${name}!`;
+}
+
+export async function waitNatsEvent(subject: string, signal: string): Promise<string> {
+  let info = Context.current().info;
+  let resp = await axios.post('http://localhost:3000/subscribe', {
+    subject: subject,
+    namespace: info.activityNamespace,
+    workflowId: info.workflowExecution.workflowId,
+    signal: signal
+  });
+  return `response=${resp.status}`;
 }
--- example/src/workflows.ts
+++ example/src/workflows.ts
@@ -1,12 +1,38 @@
 import { proxyActivities } from '@temporalio/workflow';
+import * as wf from '@temporalio/workflow';
+
 // Only import the activity types
 import type * as activities from './activities';
 
-const { greet } = proxyActivities<typeof activities>({
+const { greet, waitNatsEvent } = proxyActivities<typeof activities>({
   startToCloseTimeout: '1 minute',
 });
 
+export const unblockSignal = wf.defineSignal<[string]>('unblock');
+export const isBlockedQuery = wf.defineQuery<boolean>('isBlocked');
+
 /** A workflow that simply calls an activity */
 export async function example(name: string): Promise<string> {
-  return await greet(name);
+  let isBlocked = true;
+  let result = "";
+  wf.setHandler(unblockSignal, (payload: string) => {
+    isBlocked = false;
+    result = payload;
+  });
+  wf.setHandler(isBlockedQuery, () => isBlocked);
+
+  await greet(name);
+  await waitNatsEvent(name, 'unblock');
+
+  console.log('Blocked');
+  try {
+    await wf.condition(() => !isBlocked);
+    console.log('Unblocked', result);
+  } catch (err) {
+    if (err instanceof wf.CancelledFailure) {
+      console.log('Cancelled');
+    }
+    throw err;
+  }
+  return result;
 }
